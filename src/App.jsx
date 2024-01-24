2527dbc39a12f331d52e2e89e33ef6c0b3aaa13a
DevKalebe <bezerra.kalebe@gmail.com>
2024-01-24 17:32:33 -0300

    Componente Sidebar :lipstick:

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
-import './global.css'
+
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <>
-    <Header/>
-      <Post
-        author="Kalebe Bezerra"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium aperiam alias culpa, quaerat possimus perferendis libero accusamus fuga corrupti ut ea sequi, voluptatem modi voluptatibus,
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Kalebe Bezerra"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium aperiam alias culpa, quaerat possimus perferendis libero accusamus fuga corrupti ut ea sequi, voluptatem modi voluptatibus,
         quod officia! Veniam, itaque saepe!"
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Um post legal!"
-      />
+          />
+          <Post author="Gabriel Buzzi" content="Um post legal!" />
+        </main>
+      </div>
     </>
   );
 }
