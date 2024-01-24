edf02f131b56c17233b38e291921ab4326c56659
DevKalebe <bezerra.kalebe@gmail.com>
2024-01-24 17:58:08 -0300

    Finalizando a Sidebar :lipstick:

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import {PencilLine} from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -7,12 +9,14 @@ export function Sidebar(){
       className={styles.cover}
       src="https://images.unsplash.com/photo-1600876670541-ad960ed66b02?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/94193698?v=4" />
         <strong>Diego Fernandes</strong>
         <span>WebDeveloper</span>
       </div>
 
       <footer>
         <a href='#'>
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
